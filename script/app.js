f0115745bbb71a80790e73abd91c20beac08ae47
Daniel Merritt <daniel.mafou@gmail.com>
2023-06-23 11:50:28 -0400

    add div classes to board

--- script/app.js
+++ script/app.js
@@ -1,16 +1,28 @@
-console.log("hello")
+
+
+/* ---------- Cached Elements ----------*/
+let board = document.querySelector('.board')
+let squares = document.querySelectorAll('.board div')
+squares.forEach((square, idx) => {
+  square.classList.add(idx)
+})
+
+/* ---------- Variables ----------*/
+const startPosition = 4
+
+
+/* ---------- Event Listeners ----------*/
+
+
+
+/* ---------- Music Testing ----------*/
 const musicButton = document.getElementById("pheebs")
 musicButton.addEventListener('click', play)
 
 function play() {
- const audioElement = new Audio('../assets/themeSong/pheebs.mp3')
- audioElement.volume = 0.1
- audioElement.play()
- console.log(audioElement)
- console.log("songs")
+  const audioElement = new Audio('../assets/themeSong/pheebs.mp3')
+  audioElement.volume = 0.1
+  audioElement.play()
+  console.log(audioElement)
+  console.log("songs")
 }
-const arrayTest = [3]
-arrayTest.push(arrayTest[0]+1)
-console.log(arrayTest)
-arrayTest[0] = 2
-console.log(arrayTest)
