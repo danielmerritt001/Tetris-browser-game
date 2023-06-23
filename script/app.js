35b954f1511351633f93709e0142f133b61486ca
Daniel Merritt <daniel.mafou@gmail.com>
2023-06-23 14:04:54 -0400

    input and test reverse l piece

--- script/app.js
+++ script/app.js
@@ -8,25 +8,25 @@ squares.forEach((square, idx) => {
 })
 
 /* ---------- Variables ----------*/
-const startPosition = 13
+const startPosition = 14
 const currPosition = startPosition
 let width = 10
 
 
 /* ---------- Pieces ---------- */
 
-const lPiece0 = [1, 1-width, 1+width, 1+width+1]
-const lPiece1 = [1, 1-1, 1-1+width, 1+1]
-const lPiece2 = [1, 1-width, 1-width-1, 1+width]
-const lPiece3 = [1, 1-1, 1+1, 1+1-width]
+const lPiece0 = [0, 0-width, width, width+1]
+const lPiece1 = [0, -1, width-1, 1]
+const lPiece2 = [0, -width, -width-1, width]
+const lPiece3 = [0, -1, 1, 1-width]
 
-const lRevPiece0 = []
-const lRevPiece1 = []
-const lRevPiece2 = []
-const lRevPiece3 = []
+const lRevPiece0 = [0, -width, width, width-1]
+const lRevPiece1 = [0, 0-1, -1-width, 1]
+const lRevPiece2 = [0, -width, 0, 1-width, width]
+const lRevPiece3 = [0, -1, 1, 1+width]
 
 const lPiece =[lPiece0, lPiece1, lPiece2, lPiece3]
-const lRevPiece = []
+const lRevPiece = [lRevPiece0, lRevPiece1, lRevPiece2, lRevPiece3]
 const zPiece = []
 const sPiece = []
 const squPiece = []
@@ -36,7 +36,7 @@ const tPiece = []
 const pieces = [lPiece, lRevPiece, zPiece, sPiece, squPiece, linePiece, tPiece]
 console.log(pieces)
 
-let currPiece = pieces[0][0]
+let currPiece = pieces[1][0]
 console.log(currPiece)
 /* ---------- Event Listeners ----------*/
 
